d29f9b65e00fd09ecb20374679e613e384cf1607
mbusakwe <malibongwe.busakwe@gmail.com>
2016-07-24 21:51:42 +0200

    Initialization for the project

---
+++ UI/FileOrganizer/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mef;
+
+namespace FileOrganizer
+{
+    public class Bootstrapper: MefBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            //return Container.GetExportedValue<System.Windows.Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+        }
+
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+            //AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(Bootstrapper).Assembly));
+        }
+    }
+}
